ebc769c11b6b479ed577bef37877b8e9ba6d86ad
imran <imran@blade.speedport.ip>
2018-09-25 11:05:41 +0200

    new URL, view and template to display list items

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -49,16 +49,14 @@ class NewvisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        #inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -88,7 +86,7 @@ class NewvisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.firefox()
+        self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's
         # list
--- lists/tests.py
+++ lists/tests.py
@@ -28,16 +28,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_display_all_list_items(self):
-        Item.objects.create(text='itemey1')
-        Item.objects.create(text='itemey2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey1', response.content.decode())
-        self.assertIn('itemey2', response.content.decode())
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -54,3 +45,19 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first(ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey1')
+        Item.objects.create(text='itemey2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey1')
+        self.assertContains(response, 'itemey2')
+
+
